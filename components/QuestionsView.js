98ef3882a1ceab951701245fa163de90c9da073a
Byron H <silenceway@hotmail.com>
2018-01-08 15:44:20 -0500

    Adding Quizz logic.

--- components/QuestionsView.js
+++ components/QuestionsView.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Platform, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { Easing, View, Text, TextInput, TouchableOpacity, Platform, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { connect } from 'react-redux';
+import CustomFlipView from './CustomFlipView';
 
 class QuestionsView extends Component {
     state = {
         questions: {},
         currentQuestion: 0,
+        isFlipped: false,
+        rightAnswers: 0,
+        wrongAnswers: 0,
+        finish: false,
     }
     componentDidMount() {
         const { deck } = this.props;
@@ -14,10 +19,59 @@ class QuestionsView extends Component {
             questions: deck.questions,
         });
     }
-    onPress = () => {
+    onRightPress = () => {
         this.setState({
-            currentQuestion: this.state.currentQuestion + 1,
+            rightAnswers: this.state.rightAnswers + 1,
+        }, () => this.gotoNextQuestion());
+    }
+    onWrongPress = () => {
+        this.setState({
+            wrongAnswers: this.state.wrongAnswers + 1,
+        }, () => this.gotoNextQuestion());
+    }
+    gotoNextQuestion() {
+        const questions = this.state.questions,
+            currentQuestion = this.state.currentQuestion;
+
+        if (currentQuestion + 1 >= questions.length) {
+            this.setState({
+                finish: true,
+            });
+            return;
+        }
+
+        this.setState({
+            currentQuestion: currentQuestion + 1,
         });
+    }
+    _flip = () => {
+        this.setState({isFlipped: !this.state.isFlipped});
+    };
+    _renderFront() {
+        const questions = this.state.questions,
+            currentQuestion = this.state.currentQuestion;
+
+        return(
+        <View style={styles.cardHeader}>
+            <Text style={styles.title}>{questions[currentQuestion].question}</Text>
+            <TouchableOpacity
+                onPress={this._flip} >
+                <Text style={styles.textCenter}>Answer</Text>
+            </TouchableOpacity> 
+        </View>);
+    }
+    _renderBack() {
+        const questions = this.state.questions,
+            currentQuestion = this.state.currentQuestion;
+
+        return(
+        <View style={styles.cardHeader}>
+            <Text style={styles.answer}>{questions[currentQuestion].answer}</Text>
+            <TouchableOpacity
+                onPress={this._flip} >
+                <Text style={styles.textCenter}>Question</Text>
+            </TouchableOpacity> 
+        </View>);
     }
     render() {
         const questions = this.state.questions,
@@ -26,24 +80,38 @@ class QuestionsView extends Component {
             return (
             <View style={styles.container}>
                 <Text style={styles.counter}>{currentQuestion + 1} / {questions.length}</Text>
+                {this.state.finish ?
+                <View>
+                    <Text style={styles.title}>Finish</Text>
+                    <Text>Right: {this.state.rightAnswers}</Text>
+                    <Text>Wrong: {this.state.wrongAnswers}</Text>
+                </View> :
+                <View style={{position: 'relative', padding: 10}}>
                 {questions.length ?
                 <View>
-                <View style={styles.cardHeader}>
-                    <Text style={styles.title}>{questions[currentQuestion].question}</Text>
-                </View>
+                <CustomFlipView
+                    front={this._renderFront()}
+                    back={this._renderBack()}
+                    isFlipped={this.state.isFlipped}
+                    onFlipped={(val) => {console.log('Flipped: ' + val);}}
+                    flipAxis="y"
+                    flipEasing={Easing.out(Easing.ease)}
+                    flipDuration={500}
+                    perspective={1000}/>
                 <TouchableOpacity
-                    style={[styles.button, styles.buttonBlack]}
-                    onPress={this.onPress} >
+                    style={[styles.button, styles.buttonGreen]}
+                    onPress={this.onRightPress} >
                     <Text style={styles.textWhite}>Correct</Text>
                 </TouchableOpacity> 
                 <TouchableOpacity
-                    style={[styles.button, styles.buttonBlack]}
-                    onPress={this.onPress} >
+                    style={[styles.button, styles.buttonRed]}
+                    onPress={this.onWrongPress} >
                     <Text style={styles.textWhite}>Incorrect</Text>
                 </TouchableOpacity>
                 </View>
-            :
+                :
                 <View><Text>No questions.</Text></View>}
+                </View>}
             </View>
         );
     }
@@ -72,7 +140,13 @@ const styles = StyleSheet.create({
     },
     title: {
         textAlign: 'center',
-        fontSize: 40,
+        fontSize: 30,
+        marginBottom: 10,
+    },
+    answer: {
+        textAlign: 'center',
+        fontSize: 20,
+        marginBottom: 10,
     },
     button: {
         borderRadius: 3,
@@ -80,16 +154,19 @@ const styles = StyleSheet.create({
         margin: 10,
         alignItems: 'center',
     },
-    buttonBlack: {
-        backgroundColor: 'black',
+    buttonGreen: {
+        backgroundColor: 'green',
     },
-    buttonWhite: {
-        backgroundColor: 'white',
+    buttonRed: {
+        backgroundColor: 'red',
     },
     textWhite: {
         color: 'white',
     },
     textBlack: {
         color: 'black',
+    },
+    textCenter: {
+        textAlign: 'center',
     },
 });
